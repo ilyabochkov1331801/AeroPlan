4ec64471a64b8b5c01dc99e2fefc6804f6575b2a
Alena Nesterkina <elena.walst@bk.ru>
2021-04-25 16:44:43 +0300

    Initial Commit

---
+++ AeroPlan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AeroPlan
+//
+//  Created by Alena Nesterkina on 25.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
